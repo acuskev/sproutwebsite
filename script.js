415b89b8cf3ef57c7b51f3450fdd35fab64dc0ed
JennyTL8 <jennytl8@github.com>
2018-05-20 17:51:08 -0500

    -tabs

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function animate() {
     }
   }
 }
+
+function openTab(event, tabName){
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for(i = 0; i < tabcontent.length; i++){
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = documnet.getElementsByClassName("tablinks");
+  for(i = 0; i < tablinks.length; i++){
+    tablinks[i].className = tablinks[i].className.replace(" active","");
+  }
+
+tab
+  document.getElementById(tabName) .style.display = "block";
+  evt.currentTarget.className += " active";
+}
